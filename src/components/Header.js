b3f7068353ba171608d7adbff475c57e4379f0f8
ahmad4321 <ahmadjaved870@gmail.com>
2025-06-04 21:53:38 +0800

    Implemented HOme page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,53 @@
-import React from 'react';
+import React from "react";
 import {
   AppBar,
   Toolbar,
   Typography,
   Box,
-} from '@mui/material';
+  Link,
+} from "@mui/material";
 
 const Header = () => {
   return (
     <Box>
-      <Box
-      >
+      <Box>
         <AppBar position="static" color="transparent" elevation={0}>
-          <Toolbar sx={{ justifyContent: 'space-between' }}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Toolbar sx={{ justifyContent: "space-between" }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
               <img
                 src="http://lit-evi.hzau.edu.cn/static/RiceTraitOntology/img/RTOlogo.png"
                 alt="Logo"
                 style={{ height: 60 }}
               />
+              <Box
+                sx={{ display: "flex", alignItems: "center", paddingLeft: 50 }}
+              >
+                <Link href="/" color="inherit" underline="none">
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ display: { xs: "none", md: "flex" } }}
+                  >
+                    Home
+                  </Typography>
+                </Link>
+                <Link
+                  href="/rice-trait-ontology"
+                  color="inherit"
+                  underline="none"
+                >
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ display: { xs: "none", md: "flex" }, padding: 2 }}
+                  >
+                    Curation System
+                  </Typography>
+                </Link>
+              </Box>
             </Box>
           </Toolbar>
         </AppBar>
-        <Box sx={{ padding: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom textAlign="center">
-                A Curation System for Rice Trait Ontology
-          </Typography>
-        </Box>
       </Box>
     </Box>
   );
